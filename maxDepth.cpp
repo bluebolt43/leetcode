2e9e99141ce0a2f0bfc580312fb2f40dda3f6a0f
Ken <bluebolt43@hotmail.com>
2014-09-12 19:30:24 -0700

    Add maxDepth solution with BFS algorithm.

---
+++ maxDepth.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+};
+#define BFS 0
+#define DFS 1
+class Solution {
+public:
+#if BFS
+    int maxDepth(TreeNode *root) {
+        queue <TreeNode*> node_stack;
+        TreeNode* tmp;
+
+        int count = 0;
+		int next_level_count = 0;
+		int level = 0;
+
+        if (root == NULL){
+			return 0;
+		}
+
+		node_stack.push(root);
+		count=node_stack.size();
+		level++;
+
+		while(count>0){
+			tmp=node_stack.front();
+			if(tmp->right != NULL){
+				node_stack.push(tmp->right);
+				next_level_count++;
+			}
+			if(tmp->left != NULL){
+				node_stack.push(tmp->left);
+				next_level_count++;
+			}
+			node_stack.pop();
+			count--;
+
+			if(count==0){
+				if(next_level_count!=0){
+					count=next_level_count;
+					level++;
+					next_level_count=0;
+				}
+			}
+		}
+
+		return level;
+    }
+#endif
+#if DFS
+    int maxDepth(TreeNode *root) {
+		int left_level=0, right_level=0;
+
+        if (root == NULL){
+			return 0;
+		}
+
+		right_level = maxDepth(root->right) + 1;
+		left_level = maxDepth(root->left) + 1;
+
+		if(right_level > left_level)
+			return right_level;
+		else
+			return left_level;
+    }
+#endif
+};
+
+int main()
+{
+	TreeNode sample, left, l_r, right;
+	sample.val = 1;
+	sample.left = &left;
+	sample.right = &right;
+	left.val= 2;
+	left.left = NULL;
+	left.right = &l_r;
+	right.val = 3;
+	right.left = NULL;
+	right.right = NULL;
+	l_r.val = 4;
+	l_r.left = NULL;
+	l_r.right = NULL;
+		
+	Solution solution;
+    int i;
+	i = solution.maxDepth(&sample);
+	cout << i << endl;
+
+	return 0;
+}
