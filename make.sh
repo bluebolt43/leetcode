88207916bb035ce34d90a4e71b42dbb76e1ef2e6
Ken <bluebolt43@hotmail.com>
2014-09-29 00:20:40 -0700

    Fix a bug, script won't compiler after compiler error.

--- make.sh
+++ make.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 arg=$1
+#-std=c++0x #-std=c++11
+flag="-std=c++0x"
 if [ "x$arg" != "x" ];then
 	if [ "x$arg" != "xclean" ];then
 		echo "Usage '$0 clean' or '$0'"
 	else
 		for f in $(ls *.cpp); do
-			object=`echo $f | sed s/.cpp//g`
+			object=`echo $f | sed 's/\.cpp//g'`
 			if [ -f $object ];then
 				echo "rm $object"
 				rm $object
@@ -14,13 +16,18 @@ if [ "x$arg" != "x" ];then
 	fi
 else
 	for f in $(ls *.cpp); do
-		object=`echo $f | sed s/.cpp//g`
+		object=`echo $f | sed 's/\.cpp//g'`
 		fdate=`stat -c %Y $f`
 		objdate=`stat -c %Y $object 2>/dev/null`
 		if [ "x$fdate" != "x$objdate" ];then
-			echo "g++ $f -o $object"
-			g++ $f -o $object
-			touch -r $f $object
+			#echo "g++ $f -o $object"
+			#g++ $f -o $object
+			echo "g++ $flag $f -o $object"
+			g++ $flag $f -o $object
+
+			if [ -f $object ];then
+				touch -r $f $object
+			fi
 		fi
 	done
 fi
