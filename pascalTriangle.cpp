dbfc88e1fcf49073fa61e32d3d488ee4e5ac067b
Ken <bluebolt43@hotmail.com>
2014-10-18 12:49:17 -0700

    Pascal's Triangle II, use math to get O(n)

--- pascalTriangle.cpp
+++ pascalTriangle.cpp
@@ -32,6 +32,29 @@ public:
 		}
 		return result;
     }
+
+    vector<int> getRow(int rowIndex) {
+		int i;
+		long long tmp;
+		vector<int> result;
+		if(rowIndex<0)
+			return result;
+
+		result.push_back(1);
+		if(rowIndex==0)
+			return result;
+		result.push_back(rowIndex);
+
+		//cm:n+1=cm:n (m-n)/(n+1)  when m-n-1 >0
+		for(i=1;i<result[1];i++){
+			tmp = result[i];
+			tmp = tmp*(result[1]-i)/(i+1);
+			//cout <<result[i] <<"* " << (result[1]-i)<< "/" <<(i+1) << "="<< tmp <<endl;
+			result.push_back((int)tmp);
+		}
+
+		return result;
+    }
 };
 
 int main()
@@ -39,11 +62,19 @@ int main()
 	Solution solution;
 	vector<vector<int> > tmp;
     int i,j;
-	tmp = solution.generate(6);
+	tmp = solution.generate(10);
 	for(i=0;i<tmp.size();i++){
 		for(j=0;j<tmp[i].size();j++)
 			cout << tmp[i][j] << " ";
 		cout <<endl;
 	}
+	
+	vector<int> buf;
+	buf= solution.getRow(30);
+	for(i=0;i<buf.size();i++){
+		cout << buf[i] << " ";
+	}
+	cout <<endl;
+
 	return 0;
 }
